59961b814530c4fa7c21fd0ca15a1b2da7a8b74f
Chasingsnow <emilianomfortes@gmail.com>
2019-07-20 17:03:32 -0300

    Update juliathesis_code_list.jl

--- juliathesis_code_list.jl
+++ juliathesis_code_list.jl
@@ -1,3 +1,16 @@
+#INDEX - Use ctrl+F to browse faster
+#(1) - BINARY OPERATIONS
+#(2) - SPIN OPERATIONS
+#(3) - OUT-OF-TIME-ORDERED CORRELATORS
+##(3.1) - WITH TEMPERATURE CHOICES (IF T=INFTY USE INFTY TEMPERATURE OTOCS FOR EXTRA SPEED)
+##(3.2) - INFTY TEMPERATURE OTOCS
+#(4) - CHAOS OPERATIONS
+#(5) - MISCELLANEA CODE
+
+#----------------  (1) BINARY OPERATIONS  ----------------#
+#Translations from fortran/python --> julia
+
+#btest (https://gnu.huihoo.org/gcc/gcc-7.1.0/gfortran/BTEST.html)
 function btest(i,pos)
     a = i & (1 << pos)
     if a >= 1
@@ -6,6 +19,7 @@ function btest(i,pos)
     return Core.Bool(a)
 end
 
+#ibclr/ibset (https://gcc.gnu.org/onlinedocs/gcc-4.6.1/gfortran/IBCLR.html / http://www.lahey.com/docs/lfpro78help/F95ARIBSETFn.htm)
 function set_bit(v,index,x)
     mask = 1 << index
     v &= ~mask
