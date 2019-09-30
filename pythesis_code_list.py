bfc320c0dee6ddb44dcac00c52466ae9c3ce8225
Chasingsnow <emilianomfortes@gmail.com>
2019-09-30 17:07:23 -0300

    Update pythesis_code_list.py

--- pythesis_code_list.py
+++ pythesis_code_list.py
@@ -5,6 +5,8 @@ import math
 from scipy.optimize import curve_fit
 import timeit
 import matplotlib.pyplot as plt
+from scipy.special import erfc
+import scipy.signal
 
 #INDEX - Use ctrl+F to browse faster
 #(1) - BINARY OPERATIONS
@@ -37,28 +39,21 @@ def set_bit(v, index, x): #if x=0 --> ibclr / if x=1 --> ibset
   return v
 
 #ibits
-def ibits(num,pos,lens): 
-  
-     # convert number into binary first 
-     binary = bin(num) 
-  
-     # remove first two characters 
-     binary = binary[2:][::-1] 
-     for n in range(pos+2):
-         binary += str(0)  
-     # extract k  bit sub-string 
-     kBitSubStr = binary[pos : pos+lens][::-1] 
-     #kBitSubStr = binary[len(binary)-pos-1-lens:len(binary)-pos-1]
-     # convert extracted sub-string into decimal again 
-     return (int(kBitSubStr,2)) 
+def ibits(num,pos,lens):
+    binary = bin(num) # convert number into binary first 
+    binary = binary[2:][::-1] # remove first two characters
+    for n in range(pos+2):
+        binary +=str(0)
+    kBitSubStr = binary[pos:pos+lens][::-1]
+    return (int(kBitSubStr,2)) 
 
 def mvbits(frm,frompos,leng,to,topos):
     binary = bin(frm)[2:][::-1]
     binary2 = bin(to)[2:][::-1]
     lentot = frompos+topos+leng+max(len(binary),len(binary2))
     for m in range(lentot):
-	binary += str(0)
-	binary2 += str(0)
+        binary += str(0)
+        binary2 += str(0)
     zz = list(binary2)
     zz2 = list(binary)	 
     zz[topos:topos+leng] = zz2[frompos:frompos+leng]
@@ -145,7 +140,7 @@ def S_xxij_opt(pos_i,pos_j,sites):
     dim = 2**sites
     Sx = np.zeros((dim,dim),dtype=complex)
     for i in range(dim):
-	if (pos_i<=sites-1-np.abs(pos_j-pos_i)):
+        if (pos_i<=sites-1-np.abs(pos_j-pos_i)):
             t1 = (i)^(set_bit(0,pos_i,1))
             t1 = t1^(set_bit(0,(pos_i+np.abs(pos_j-pos_i) % sites), 1))
             Sx[i,t1]+= 1
@@ -176,7 +171,7 @@ def S_yyij_opt(pos_i,pos_j,sites):
     dim = 2**sites
     S = np.zeros((dim,dim),dtype=complex)
     for i in range(dim):
-	if (pos_i<=sites-1-np.abs(pos_j-pos_i)):
+        if (pos_i<=sites-1-np.abs(pos_j-pos_i)):
             t1 = (i)^(set_bit(0,pos_i,1))
             t1 = t1^(set_bit(0,(pos_i+np.abs(pos_j-pos_i) % sites), 1))
             S[i,t1]+= -1*((-1)**(ibits(i,pos_i,1) + ibits(i,pos_i+np.abs(pos_j-pos_i) % sites,1)))
@@ -213,7 +208,7 @@ def S_zzij_opt(pos_i,pos_j,sites):
     dim = 2**sites
     S = np.zeros((dim,dim),dtype=complex)
     for i in range(dim):
-	if (pos_i<=sites-1-np.abs(pos_j-pos_i)):
+         if (pos_i<=sites-1-np.abs(pos_j-pos_i)):
             S[i,i]+= (-1)**(ibits(i,pos_i,1) + ibits(i,pos_i+np.abs(pos_j-pos_i) % sites,1))
     return S
 
@@ -286,17 +281,17 @@ def spin_interactions(sites,neig,BC,Cxx,Cyy,Czz):
     t1 = 0
     kk = 0
     if (type(Cxx) == float) | (type(Cxx) == int):
-	Cx = np.zeros(dim,dtype=float)+Cxx
+        Cx = np.zeros(dim,dtype=float)+Cxx
     else:
-	Cx = Cxx
+        Cx = Cxx
     if (type(Cyy) == float) | (type(Cyy) == int):
-	Cy = np.zeros(dim,dtype=float)+Cyy
+        Cy = np.zeros(dim,dtype=float)+Cyy
     else:
-	Cy = Cyy
+        Cy = Cyy
     if (type(Czz) == float) | (type(Czz) == int):
-	Cz = np.zeros(dim,dtype=float)+Czz
+        Cz = np.zeros(dim,dtype=float)+Czz
     else:
-	Cz = Czz
+        Cz = Czz
     for i in range(dim):
         for n in range(sites-1):
             if ((n<=sites-1-neig) | (BC=="perodic")):
@@ -317,6 +312,12 @@ def Tilted_model(sites,BC,J,B,tita):
     e, ev = np.linalg.eigh(H)
     return e, ev
 
+def Ising_XZ_fields(sites,BC,J,hx,hy,hz):
+    H = -J*spin_interactions(sites,1,BC,0,0,1)
+    H+= (hx*S_x(sites) + hy*S_y(sites) + hz*S_z(sites))
+    e, ev = np.linalg.eigh(H)
+    return e, ev
+
 def Perturbed_XXZ(sites,BC,alpha,lambd):
     H = .25*spin_interactions(sites,1,BC,1,1,alpha)
     H+= .25*lambd*spin_interactions(sites,2,BC,1,1,alpha)
@@ -327,42 +328,105 @@ def Perturbed_XXZ(sites,BC,alpha,lambd):
 
 ###(2.3.1) PARITY
 
+# "EVEN" and "ODD" for either desired subspace. If "BOTH" (or anything else) returns the basis and energies ordered from even to odd parity and smaller to higher energies
 def parity_subspace(sites,basis,ener,pariti):
     dim = 2**sites
     basis2 = np.zeros((dim,dim),dtype=complex)
     if sites % 2 == 0:
-	rangoz = int(sites/2)
+        rangoz = int(sites/2)
     else:
-	rangoz = int((sites-1)/2)
+        rangoz = int((sites-1)/2)
     #print(rangoz)
     for i in range(dim):
-	q = i
-	p = i
-	for j in range(rangoz):
-	    q = mvbits(p,j,1,q,sites-1-j)
-	    #print(q)
+        q = i
+        p = i
+        for j in range(rangoz):
+            q = mvbits(p,j,1,q,sites-1-j)
+            #print(q)
             q = mvbits(p,sites-1-j,1,q,j)
-	    #print(q)
-	basis2[i,:] = basis[q,:]
+            #print(q)
+        basis2[i,:] = basis[q,:]
     dimparity = -1
     enerparity = []
     basisparity = np.zeros((dim,dim),dtype=complex)
     if pariti == "EVEN":
-	for i in range(dim):
-	    #print(np.vdot(basis[:,i],basis2[:,i]).real)
-	    if np.vdot(basis[:,i],basis2[:,i]).real > 0:
-		dimparity += 1
-		enerparity.append(ener[i])
-		basisparity[:,dimparity]=basis[:,i]
-    else:
-	for i in range(dim):
- 	    #print(np.vdot(basis[:,i],basis2[:,i]).real)
-	    if np.vdot(basis[:,i],basis2[:,i]).real < 0:
-		dimparity += 1
-		enerparity.append(ener[i])
-		basisparity[:,dimparity]=basis[:,i]
+        for i in range(dim):
+            #print(np.vdot(basis[:,i],basis2[:,i]).real)
+            if np.vdot(basis[:,i],basis2[:,i]).real > 0:
+                dimparity += 1
+                enerparity.append(ener[i])
+                basisparity[:,dimparity]=basis[:,i]
+    if pariti == "ODD":
+        for i in range(dim):
+            #print(np.vdot(basis[:,i],basis2[:,i]).real)
+            if np.vdot(basis[:,i],basis2[:,i]).real < 0:
+                dimparity += 1
+                enerparity.append(ener[i])
+                basisparity[:,dimparity]=basis[:,i]
+    if pariti == "BOTH":
+        for i in range(dim):
+            #print(np.vdot(basis[:,i],basis2[:,i]).real)
+            if np.vdot(basis[:,i],basis2[:,i]).real > 0:
+                dimparity += 1
+                enerparity.append(ener[i])
+                basisparity[:,dimparity]=basis[:,i]
+        for i in range(dim):
+            #print(np.vdot(basis[:,i],basis2[:,i]).real)
+            if np.vdot(basis[:,i],basis2[:,i]).real < 0:
+                dimparity += 1
+                enerparity.append(ener[i])
+                basisparity[:,dimparity]=basis[:,i]
     return enerparity,basisparity[:,0:dimparity+1]
 
+# Reorders the eigenkets and eigenstates in the first even block and second odd block	
+def parity_subspace_both(sites,basis,ener):
+    dim = 2**sites
+    basis2 = np.zeros((dim,dim),dtype=complex)
+    if sites % 2 == 0:
+        rangoz = int(sites/2)
+    else:
+        rangoz = int((sites-1)/2)
+    #print(rangoz)
+    for i in range(dim):
+        q = i
+        p = i
+        for j in range(rangoz):
+            q = mvbits(p,j,1,q,sites-1-j)
+            q = mvbits(p,sites-1-j,1,q,j)
+        basis2[i,:] = basis[q,:]
+    dimparity = -1
+    enerparity = []
+    basisparity = np.zeros((dim,dim),dtype=complex)
+    for i in range(dim):
+        if np.vdot(basis[:,i],basis2[:,i]).real > 0:
+            dimparity += 1
+            enerparity.append(ener[i])
+            basisparity[:,dimparity]=basis[:,i]
+    dimeven = len(enerparity)
+    for i in range(dim):
+        if np.vdot(basis[:,i],basis2[:,i]).real < 0:
+            dimparity += 1
+            enerparity.append(ener[i])
+            basisparity[:,dimparity]=basis[:,i]
+    return enerparity,basisparity[:,0:dimparity+1],dimeven
+
+	
+# Operator 1/2 (S_zi+S_z(sites-i)) which commutes with parity!
+def S_zi_parity(pos_i,sites,basisparity):
+    dim = 2**sites
+    dim2 = min(basisparity.shape)
+    S = np.zeros((dim2,dim2),dtype=complex)
+    if (sites % 2 != 0 and pos_i == int(sites/2 - 0.5) ):
+        for i in range(dim2):
+            for j in range(dim2):
+                for m in range(dim):
+                    S[i,j] += basisparity[m,i]*basisparity[m,j]* ((-1)**(ibits(m,pos_i,1))) 
+    else:
+        for i in range(dim2):
+            for j in range(dim2):
+                for m in range(dim):
+                    S[i,j] += 0.5*basisparity[m,i]*basisparity[m,j]*  ( ((-1)**(ibits(m,pos_i,1))) + ((-1)**(ibits(m,sites-1-pos_i,1))) )    
+    return S
 
 ###(2.3.2) S_z CONSERVATION
 
@@ -407,9 +471,9 @@ def sz_subspace_S_xxij_opt(pos_i,pos_j,sites,n_partic):
     S = np.zeros((dim,dim),dtype=complex)
     states, flag = sz_subspace(sites,n_partic)
     for i in range(dim):
-	if (pos_i<=sites-1-np.abs(pos_j-pos_i)):
+        if (pos_i<=sites-1-np.abs(pos_j-pos_i)):
             stepi = ibits(states[i],pos_i,1) + ibits(states[i], (pos_i+np.abs(pos_j-pos_i))%(sites),1)
-	    if stepi == 1:
+            if stepi == 1:
                 t1 = (states[i])^(set_bit(0,pos_i,1))
                 print(t1)
                 t1 = int(flag[(t1)^(set_bit(0, pos_i+np.abs(pos_j-pos_i) % sites, 1))] )
@@ -443,9 +507,9 @@ def sz_subspace_S_yyij_opt(pos_i,pos_j,sites,n_partic):
     S = np.zeros((dim,dim),dtype=complex)
     states, flag = sz_subspace(sites,n_partic)
     for i in range(dim):
-	if (pos_i<=sites-1-np.abs(pos_j-pos_i)):
+        if (pos_i<=sites-1-np.abs(pos_j-pos_i)):
             stepi = ibits(states[i],pos_i,1) + ibits(states[i], (pos_i+np.abs(pos_j-pos_i))%(sites),1)
-	    if stepi == 1:
+            if stepi == 1:
                 t1 = (states[i])^(set_bit(0,pos_i,1))
                 print(t1)
                 t1 = int(flag[(t1)^(set_bit(0, pos_i+np.abs(pos_j-pos_i) % sites, 1))] )
@@ -461,17 +525,17 @@ def sz_subspace_S_yyij(pos_i,pos_j,sites,n_partic):
     for i in range(dim):
         if btest(states[i],pos_i) == True:
             estados2[i] = set_bit(states[i],pos_i,0)
-	    a[i] = 1j
+            a[i] = 1j
         else:
             estados2[i] = set_bit(states[i],pos_i,1)
-	    a[i] = -1j
+            a[i] = -1j
     for i in range(dim):
         if btest(estados2[i],pos_j) == True:
             estados2[i] = set_bit(estados2[i],pos_j,0)
             a[i] = a[i]*1j
         else:
             estados2[i] = set_bit(estados2[i],pos_j,1)
-	    a[i] = -a[i]*1j
+            a[i] = -a[i]*1j
     for i in range(dim):
         for j in range(dim):
             if states[i] == estados2[j]:
@@ -484,7 +548,7 @@ def sz_subspace_S_zzij_opt(pos_i,pos_j,sites,n_partic):
     S = np.zeros((dim,dim),dtype=complex)
     states, flag = sz_subspace(sites,n_partic)
     for i in range(dim):
-	if (pos_i<=sites-1-np.abs(pos_j-pos_i)):
+        if (pos_i<=sites-1-np.abs(pos_j-pos_i)):
             S[i,i]+= (-1)**(ibits(states[i],pos_i,1) + ibits(states[i],pos_i+np.abs(pos_j-pos_i) % sites,1))
     return S
 
@@ -495,12 +559,12 @@ def sz_subspace_S_zzij(pos_i,pos_j,sites,n_partic):
     for i in range(dim):
         if btest(states[i],pos_i) == True:
             if btest(states[i],pos_j) == True:            
-	    	S[i,i]+=1
+                S[i,i]+=1
             else:
             	S[i,i]+=-1
-	else:
-	    if btest(states[i],pos_j) == True:            
-	    	S[i,i]+=-1
+        else:
+            if btest(states[i],pos_j) == True:            
+                S[i,i]+=-1
             else:
             	S[i,i]+=1
     return S
@@ -514,17 +578,17 @@ def sz_subspace_spin_interactions(sites,n_partic,neig,BC,Cxx,Cyy,Czz):
     t1 = 0
     kk = 0
     if (type(Cxx) == float) | (type(Cxx) == int):
-	Cx = np.zeros(dim,dtype=float)+Cxx
+        Cx = np.zeros(dim,dtype=float)+Cxx
     else:
-	Cx = Cxx
+        Cx = Cxx
     if (type(Cyy) == float) | (type(Cyy) == int):
-	Cy = np.zeros(dim,dtype=float)+Cyy
+        Cy = np.zeros(dim,dtype=float)+Cyy
     else:
-	Cy = Cyy
+        Cy = Cyy
     if (type(Czz) == float) | (type(Czz) == int):
-	Cz = np.zeros(dim,dtype=float)+Czz
+        Cz = np.zeros(dim,dtype=float)+Czz
     else:
-	Cz = Czz
+        Cz = Czz
     #print(flag)
     for i in range(1,dim+1):
         for n in range(sites-1):
@@ -687,6 +751,7 @@ def OTOCF_opt_infty(V,W,ener,basis,N,dt,t0,ortho):
     otok = np.zeros(N,dtype = "float32")
     U = np.zeros((dim,dim),dtype="complex64",order='F') # U evolucion temporal
     Udagger = np.zeros((dim,dim),dtype="complex64",order='F') # U*
+    tiempo = np.linspace(t0,t0+N*dt,N)
     for ti in range(0,N):
         for c1 in range(0,dim):
             U[c1][c1] = np.exp(-1j*tiempo[ti]*ener[c1])
@@ -698,6 +763,29 @@ def OTOCF_opt_infty(V,W,ener,basis,N,dt,t0,ortho):
         otok[ti] = 1 - (np.matrix.trace(mm)/len(ener)).real 
     otok = np.array(otok)
     return otok
+
+def OTOCF_opt_infty_nobasischange(V,W,ener,N,dt,t0):
+    S0 = np.complex64(V, order = 'F')
+    S = np.complex64(W, order = 'F')
+    dim = len(ener)
+    mm = np.zeros((dim,dim),dtype="complex64",order='F')
+    mm1 = np.zeros((dim,dim),dtype="complex64",order='F')
+    otok = np.zeros(N,dtype = "float32")
+    U = np.zeros((dim,dim),dtype="complex64",order='F') # U evolucion temporal
+    Udagger = np.zeros((dim,dim),dtype="complex64",order='F') # U*
+    tiempo = np.linspace(t0,t0+N*dt,N)
+    for ti in range(0,N):
+        for c1 in range(0,dim):
+            U[c1][c1] = np.exp(-1j*tiempo[ti]*ener[c1])
+            Udagger[c1][c1] = np.exp(1j*tiempo[ti]*ener[c1])
+        mm = FB.cgemm(1,S0,U)
+        mm1= FB.cgemm(1,Udagger,mm)
+        mm = FB.cgemm(1,mm1,S)
+        mm = FB.cgemm(1,mm,mm)
+        otok[ti] = 1 - (np.matrix.trace(mm)/len(ener)).real 
+    otok = np.array(otok)
+    return otok
+
 
 # BOTH OTOCS 1 and 2 (REQUIRES A MORE DETAILED DESCRIPTION)
 def OTOCS_opt_infty(V,W,ener,basis,N,dt,t0,ortho):
@@ -793,10 +881,56 @@ def OTOCF_infty(V,W,ener,basis,N,dt,t0,ortho):
         mm1= np.matmul(Udagger,mm)
         mm = np.matmul(mm1,S)
         mm = np.matmul(mm,mm)
-        otok[ti] = 1 - (np.matrix.trace(mm)/len(ener)).real 
+        otok[ti] = 1 - (np.matrix.trace(mm)/dim).real 
     otok = np.array(otok)
     return otok
+#----------------  (4) 4 POINT OTOCS  ----------------#
 
+def OTOC4F_opt_infty(A,B,C,D,ener,basis,N,dt,t0,ortho):
+    basis = np.complex64(basis, order = 'F')
+    A = np.complex64(A, order = 'F')
+    B = np.complex64(B, order = 'F')
+    C = np.complex64(C, order = 'F')
+    D = np.complex64(D, order = 'F')
+    dim = len(ener)
+    if ortho == True:
+        basist = np.transpose(basis)
+    else:
+        basist = np.linalg.inv(basis)
+    A = FB.cgemm(1,A,basis)
+    A = FB.cgemm(1,basist,A)
+    B = FB.cgemm(1,B,basis)
+    B = FB.cgemm(1,basist,B)
+    C = FB.cgemm(1,C,basis)
+    C = FB.cgemm(1,basist,C)
+    D = FB.cgemm(1,D,basis)
+    D = FB.cgemm(1,basist,D)
+    mmc = np.zeros((dim,dim),dtype="complex64",order='F')
+    mmd = np.zeros((dim,dim),dtype="complex64",order='F')
+    mm = np.zeros((dim,dim),dtype="complex64",order='F')
+    mm1 = np.zeros((dim,dim),dtype="complex64",order='F')
+    tiempo = np.linspace(t0,N*dt+t0,N)
+    otok = np.zeros(N,dtype = "float32")
+    U = np.zeros((dim,dim),dtype="complex64",order='F') # U evolucion temporal
+    Udagger = np.zeros((dim,dim),dtype="complex64",order='F') # U*
+    for ti in range(0,N):
+        for c1 in range(0,dim):
+            U[c1][c1] = np.exp(-1j*tiempo[ti]*ener[c1])
+            Udagger[c1][c1] = np.exp(1j*tiempo[ti]*ener[c1])
+        mmc = FB.cgemm(1,C,U)
+        mmc= FB.cgemm(1,Udagger,mmc)
+        mmd = FB.cgemm(1,D,U)
+        mmd= FB.cgemm(1,Udagger,mmd)
+        mm = FB.cgemm(1,A,B)
+        mm1 = FB.cgemm(1,mmc,mmd)
+        mm = FB.cgemm(1,mm,mm1)       
+        otok[ti] = np.matrix.trace(mm)/dim       
+        mm = FB.cgemm(1,A,mmc)
+        mm1 = FB.cgemm(1,B,mmd)
+        mm = FB.cgemm(1,mm,mm1)
+        otok[ti] += - (np.matrix.trace(mm)/dim).real
+    otok = np.array(otok)
+    return otok
 #----------------  (4) CHAOS OPERATIONS  ----------------#
 
 #Level statistics/ NN level spacing distribution
@@ -840,9 +974,11 @@ def Brody_distribution(s,B):
     return (B+1)*bebi*(s**B) * np.exp(-bebi*(s**(B+1)))
 
 # BERRY ROBNIK DISTRIBUTION 
-def BerryRobnik_distribution(s,Q):
-    bebi = (Q+0.5*np.pi*(1-Q)*s)    
-    return bebi * np.exp(-Q*s - 0.25*np.pi*(1-Q)*s*s)
+def BerryRobnikN2_distribution(s,p):
+    p1 = 1 - p
+    a = (p1)**2 * np.exp(-p1*s) * erfc(0.5*np.sqrt(np.pi) *p*s)
+    b = (2*p1*p+0.5*np.pi*(p**3)*s) * np.exp(-p1*s-0.25*np.pi*(p**2)*(s**2))    
+    return a+b
 
 
 # Adjusts the Brody parameter to a discrete distribution of energy "yyy" separated in an amount bins "beens" (Recommended beens='auto')
@@ -873,7 +1009,7 @@ def brody_param(yyy,beens):
     return brody_paramet
 
 # Adjusts the BerryRobnik parameter to a discrete distribution of energy "yyy" separated in an amount bins "beens" (Recommended beens='auto')
-def BerryRobnik_param(yyy,beens):
+def BerryRobnikN2_param(yyy,beens):
     med1=0
     for count2 in range(0,len(yyy)):
         med1=med1+yyy[count2]
@@ -896,21 +1032,32 @@ def BerryRobnik_param(yyy,beens):
     pruebas = np.zeros(len(prueba)-1)
     for lau in range(1,len(prueba)):
         pruebas[lau-1] = lau * deltilla
-    BR_paramet, pcov = curve_fit(BerryRobnik_distribution, pruebas, datos)
+    BR_paramet, pcov = curve_fit(BerryRobnikN2_distribution, pruebas, datos)
     return BR_paramet
+
 
 # Calcultes r parameter in the 10% center of the energy "ener" spectrum. If plotadjusted = True, returns the magnitude adjusted to Poisson = 0 or WD = 1
 def r_chaometer(ener,plotadjusted):
     ra = np.zeros(len(ener)-2)
-    center = int(0.5*len(ener))
-    delter = int(0.1*len(ener))
+    #center = int(0.5*len(ener))
+    #delter = int(0.05*len(ener))
     for ti in range(len(ener)-2):
         ra[ti] = (ener[ti+2]-ener[ti+1])/(ener[ti+1]-ener[ti])
         ra[ti] = min(ra[ti],1.0/ra[ti])
-    ra = np.mean(ra[center-delter:center+delter])
+    ra = np.mean(ra)
     if plotadjusted == True:
         ra = (ra -0.3863) / (-0.3863+0.5307)
     return ra
+    
+# Calculates the NPC of eigenvectors in comparison to the standard spin-site basis
+def IPR_eigenstates(basis):
+    dim1 = min(basis.shape)
+    dim2 = max(basis.shape)
+    ipr = np.zeros(dim1)
+    for i in range(dim1):
+        for j in range(dim2):
+            ipr[i]+= (np.abs(basis[j,i]))**4
+    return ipr
 
 # Returns OTOC's Spectarl IPR and Standard deviation.
 def OTOC_chaotic_measures(tiempo,otoks,lentest0,lentest,dt):
@@ -934,6 +1081,28 @@ def OTOC_chaotic_measures(tiempo,otoks,lentest0,lentest,dt):
     #sipr_df = 1/sipr_df
     sipr_simpson = 1/sipr_simpson
     return sipr_simpson, std
+    
+# Returns OTOC's Spectarl IPR and Standard deviation with a more sofisticated approach.
+def OTOC_chaotic_measures_soph(tiempo,otoks,largo,largoz,N,dt,WINDOWTYPE):
+    largofinal = int((N - int(N/(largo)))/largoz) 
+    std = 0
+    sipr_simpson = 0
+    for lar in range(largofinal):
+        lentest0 = int((lar*largoz))
+        lentest = int(N/(largo))+int((lar*largoz))
+        distri = 1
+        otok_osc = otoks[lentest0:lentest] - np.mean(otoks[lentest0:lentest])
+        otok_osc = otok_osc * scipy.signal.windows.get_window(WINDOWTYPE, len(otok_osc))
+        std += np.std(otok_osc)
+        fft_osc = np.fft.rfft(otok_osc,norm='ortho')
+        freq = np.fft.rfftfreq(len(otok_osc), tiempo[1]-tiempo[0])
+        dx = freq[1]-freq[0]
+        b = 0.5*len(freq)-1
+        distri = scipy.integrate.simps(np.abs(fft_osc)**2,freq)
+        sipr_simpson += scipy.integrate.simps(np.abs(fft_osc)**4/(distri**2),freq)
+    sipr_simpson = 1.0/sipr_simpson
+    std = 1.0/std
+    return sipr_simpson, std
 
 
 #----------------  (5) MISCELLANEA CODE  ----------------#
@@ -955,3 +1124,4 @@ def angle_between(v1, v2):
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
